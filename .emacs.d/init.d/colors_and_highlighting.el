88c66fe281f5dbf9f5b79fdbc39c01608d3aa7a8
John Cinnamond <jc@panagile.com>
2013-05-08 08:39:12 +0100

    Use highline for better current line highlighting. Increase 80+ contrast.

--- .emacs.d/init.d/colors_and_highlighting.el
+++ .emacs.d/init.d/colors_and_highlighting.el
@@ -7,19 +7,21 @@
 (transient-mark-mode 1)
 (set-face-background 'region "#707")
 
-(global-hl-line-mode 1)
+;; Use highline to highlight the current line. This doesn't trample on other
+;; highlighting.
+(jc-ensure-package 'highline)
+(require 'highline)
+(set-face-background 'highline-face "#333")
+(global-highline-mode 1)
 
 ;; Whitespace
 (global-whitespace-mode 1)
 (setq whitespace-style (quote
 			    ( face trailing tabs lines-tail tab-mark )))
 (set-face-background 'whitespace-tab "red")
+(set-face-foreground 'whitespace-line "#77f")
+(set-face-background 'whitespace-line "#522")
 (add-hook 'before-save-hook 'whitespace-cleanup)
-
-;; Highlight long lines
-(jc-ensure-package 'highlight-80+)
-(require 'highlight-80+)
-(highlight-80+-mode)
 
 ;; Highlight matching parentheses
 (jc-ensure-package 'highlight-parentheses)
