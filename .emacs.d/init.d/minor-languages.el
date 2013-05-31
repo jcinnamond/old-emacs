31d0114a133681162e43f018b710491c16526550
John Cinnamond <jc@panagile.com>
2013-05-31 08:21:51 +0100

    Better css and javascript support.

--- .emacs.d/init.d/minor-languages.el
+++ .emacs.d/init.d/minor-languages.el
@@ -3,3 +3,13 @@
 
 (jc-ensure-package 'mustache-mode)
 (require 'mustache-mode)
+
+(jc-ensure-package 'scss-mode)
+(require 'scss-mode)
+(setq scss-compile-at-save nil)
+
+;; Javascript
+(jc-ensure-package 'js2-mode)
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
