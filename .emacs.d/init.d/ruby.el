ef99f3916ec51fa42596bb435d0e087e14603b08
John Cinnamond <jc@panagile.com>
2013-05-31 09:13:13 +0100

    Switch to ruby test_unit (hopefully temporarily)

--- .emacs.d/init.d/ruby.el
+++ .emacs.d/init.d/ruby.el
@@ -21,17 +21,23 @@
 (jc-ensure-package 'yaml-mode)
 (require 'yaml-mode)
 
-;; Rspec support
-(jc-ensure-package 'rspec-mode)
-(require 'rspec-mode)
-(setq rspec-use-rake-flag nil)
+;; Test unit support - bleugh
+(jc-ensure-package 'ruby-test-mode)
+(require 'ruby-test-mode)
+(add-to-list 'auto-mode-alist '("_test\\.rb" . ruby-test-mode))
+(setq ruby-test-ruby-executables '("ruby" "ruby1.9"))
 
-;; Load rspec mode with enhanced-ruby-mode
-(add-hook 'enh-ruby-mode-hook (lambda ()
-				(if (rspec-buffer-is-spec-p)
-				    (rspec-mode)
-				  (rspec-verifiable-mode))
-				))
+;; Rspec support - DISABLED while I use test unit :-(
+(jc-ensure-package 'rspec-mode)
+;; (require 'rspec-mode)
+;; (setq rspec-use-rake-flag nil)
+
+;; Load rspec mode with enhanced-ruby-mode - DISABLED while I use test unit :-(
+;; (add-hook 'enh-ruby-mode-hook (lambda ()
+;; 				(if (rspec-buffer-is-spec-p)
+;; 				    (rspec-mode)
+;; 				  (rspec-verifiable-mode))
+;; 				))
 
 ;; Helpers for converting string -> symbol
 (jc-ensure-package 'ruby-tools)
@@ -41,20 +47,21 @@
 (add-hook 'enh-ruby-mode-hook 'highlight-parentheses-mode)
 
 ;; Taken from http://blog.senny.ch/blog/2012/10/06/emacs-tidbits-for-ruby-developers/
-(defun senny-ruby-open-spec-other-buffer ()
-  (interactive)
-  (when (featurep 'rspec-mode)
-    (let ((source-buffer (current-buffer))
-          (other-buffer (progn
-                          (rspec-toggle-spec-and-target)
-                          (current-buffer))))
-      (switch-to-buffer source-buffer)
-      (pop-to-buffer other-buffer))))
+;; DISABLED while I use test unit :-(
+;; (defun senny-ruby-open-spec-other-buffer ()
+;;   (interactive)
+;;   (when (featurep 'rspec-mode)
+;;     (let ((source-buffer (current-buffer))
+;;           (other-buffer (progn
+;;                           (rspec-toggle-spec-and-target)
+;;                           (current-buffer))))
+;;       (switch-to-buffer source-buffer)
+;;       (pop-to-buffer other-buffer))))
 
-;; Load rspec mode with enhanced-ruby-mode
-(add-hook 'enh-ruby-mode-hook (lambda ()
-				(define-key enh-ruby-mode-map (kbd "C-c , ,") 'senny-ruby-open-spec-other-buffer)
-				))
+;; ;; Load rspec mode with enhanced-ruby-mode
+;; (add-hook 'enh-ruby-mode-hook (lambda ()
+;; 				(define-key enh-ruby-mode-map (kbd "C-c , ,") 'senny-ruby-open-spec-other-buffer)
+;; 				))
 
 ;; Use outline mode for basic code folding
 (add-hook 'enh-ruby-mode-hook (lambda()
