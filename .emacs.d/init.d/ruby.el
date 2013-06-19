d375a5e8897f493f7a885582979488d2007a26d1
John Cinnamond <jc@panagile.com>
2013-06-19 08:07:27 +0100

    Use enh-ruby-mode for rakefiles

--- .emacs.d/init.d/ruby.el
+++ .emacs.d/init.d/ruby.el
@@ -3,6 +3,7 @@
 (autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 
 ;; Rbenv support
