cbb57831918c5d4ac88a1d0c24c3fbc6bd321700
John Cinnamond <jcinnamond@morel.local>
2013-06-25 17:58:34 +0100

    Removed git gutter

--- .emacs.d/init.d/git.el
+++ .emacs.d/init.d/git.el
@@ -1,10 +1,2 @@
 (jc-ensure-package 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
-
-(jc-ensure-package 'git-gutter)
-(require 'git-gutter)
-;; Setting git gutter mode globally conflicts with magit.
-(global-set-key (kbd "C-c g") 'git-gutter-mode)
-
-(jc-ensure-package 'git-gutter-fringe)
-(require 'git-gutter-fringe)
