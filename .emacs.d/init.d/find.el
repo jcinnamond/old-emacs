ac4184569d065739aaf1a5d1bbe67c48c0b50b80
John Cinnamond <jc@panagile.com>
2013-08-02 19:16:14 +0100

    ido improvements

--- .emacs.d/init.d/find.el
+++ .emacs.d/init.d/find.el
@@ -4,12 +4,17 @@
 ;; Enhance ido
 (jc-ensure-package 'ido-better-flex)
 (ido-better-flex/enable)
+(setq ido-enable-flex-matching t)
 
 (jc-ensure-package 'ido-yes-or-no)
 (require 'ido-yes-or-no)
 
 (jc-ensure-package 'ido-ubiquitous)
 (require 'ido-ubiquitous)
+(ido-ubiquitous-mode)
+
+;; Stop ido jumping directories when trying to create a new file
+(setq ido-auto-merge-work-directories-length -1)
 
 ;; Find files even if you don't know what directory they are in
 (jc-ensure-package 'find-things-fast)
