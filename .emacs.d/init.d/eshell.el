0ec86af959c8e4e8ebe13372db61c4585357f34b
John Cinnamond <jc@panagile.com>
2013-08-02 19:17:06 +0100

    eshell config

---
+++ .emacs.d/init.d/eshell.el
@@ -0,0 +1,25 @@
+(require 'eshell)
+(require 'em-smart)
+(setq eshell-where-to-jump 'begin)
+(setq eshell-review-quick-commands nil)
+(setq eshell-smart-space-goes-to-end nil)
+
+(add-hook 'eshell-mode-hook (lambda()
+			      (eshell-smart-initialize)))
+
+(defun eshell/clear ()
+  "Clear the eshell buffer."
+  (interactive)
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (comint-send-input)))
+
+(jc-ensure-package 'esh-buf-stack)
+(require 'esh-buf-stack)
+(setup-eshell-buf-stack)
+(add-hook 'eshell-mode-hook (lambda()
+			      (local-set-key (kbd "M-q") 'eshell-push-command)))
+
+(jc-ensure-package 'shell-switcher)
+(require 'shell-switcher)
+(setq shell-switcher-mode t)
