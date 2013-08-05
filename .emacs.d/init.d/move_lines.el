2536f69013167481cc3ca206fc62ed132db4fb2f
John Cinnamond <jc@panagile.com>
2013-08-05 09:51:47 +0100

    Added move-lines

---
+++ .emacs.d/init.d/move_lines.el
@@ -0,0 +1,13 @@
+(defun move-line-up ()
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2))
+
+(defun move-line-down ()
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1))
+
+(global-set-key (kbd "M-<up>") 'move-line-up)
+(global-set-key (kbd "M-<down>") 'move-line-down)
