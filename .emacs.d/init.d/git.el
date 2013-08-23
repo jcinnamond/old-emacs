6761a87bf59f900f5778df540b45bc328950c777
John Cinnamond <jc@panagile.com>
2013-08-23 19:04:51 +0100

    Require the git-modes (broken out from magit)

--- .emacs.d/init.d/git.el
+++ .emacs.d/init.d/git.el
@@ -1,2 +1,6 @@
 (jc-ensure-package 'magit)
+(jc-ensure-package 'git-commit-mode)
+(jc-ensure-package 'git-rebase-mode)
+(jc-ensure-package 'gitconfig-mode)
+(jc-ensure-package 'gitignore-mode)
 (global-set-key (kbd "C-x g") 'magit-status)
