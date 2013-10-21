850b22d842e93b5041bb0c936b5638cea7a78794
John Cinnamond <jc@panagile.com>
2013-10-21 19:11:35 +0100

    Fixed key binding to move lines

--- .emacs.d/init.d/move_lines.el
+++ .emacs.d/init.d/move_lines.el
@@ -9,5 +9,5 @@
   (transpose-lines 1)
   (forward-line -1))
 
-(global-set-key (kbd "M-<up>") 'move-line-up)
-(global-set-key (kbd "M-<down>") 'move-line-down)
+(global-set-key (kbd "M-S-<up>") 'move-line-up)
+(global-set-key (kbd "M-S-<down>") 'move-line-down)
