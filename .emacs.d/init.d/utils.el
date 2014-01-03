1fb8360df1c548ba0d9169be0c7a56252655d8b6
John Cinnamond <jc@panagile.com>
2014-01-03 09:21:07 +0000

    Added 'touch' (useful for triggering tests).

--- .emacs.d/init.d/utils.el
+++ .emacs.d/init.d/utils.el
@@ -3,3 +3,11 @@
   (let ((face (or (get-char-property (point) 'read-face-name)
                   (get-char-property (point) 'face))))
     (if face (message "Face: %s" face) (message "No face at %d" pos))))
+
+(defun touch ()
+     "updates mtime on the file for the current buffer"
+     (interactive)
+     (shell-command (concat "touch " (shell-quote-argument (buffer-file-name))))
+     (message (concat "Touched " (buffer-file-name)))
+     (clear-visited-file-modtime))
+(global-set-key (kbd "C-x t") 'touch)
