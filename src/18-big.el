793b77ba57134cc505109b23a96ac435109f10b3
John Cinnamond <jc@panagile.com>
2015-04-19 22:06:59 +0100

    Simplify mode line in 'big' mode

--- src/18-big.el
+++ src/18-big.el
@@ -9,8 +9,25 @@
 
 (defun big/off ()
   (set-face-attribute 'default nil :height 150)
-  (setq big-state nil))
+  (setq big-state nil)
+
+  ;; Re-enable distractions in the mode line
+  (display-battery-mode 1)
+  (display-time-mode 1)
+  (column-number-mode 1)
+  (line-number-mode 1)
+  (setq mode-line-format big-previous-mode-line-format))
 
 (defun big/on ()
   (set-face-attribute 'default nil :height 240)
-  (setq big-state 't))
+  (setq big-state t)
+
+  ;; Remember mode line configuration so we can restore it later
+  (setq big-previous-mode-line-format mode-line-format)
+
+  ;; Hide distractions in the mode line
+  (display-battery-mode 0)
+  (display-time-mode 0)
+  (column-number-mode 0)
+  (line-number-mode 0)
+  (setq mode-line-format "%m"))
