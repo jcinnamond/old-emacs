a5fdd5ec7a00a72e2b220518fbd6b3ffc3ea3678
John Cinnamond <jc@panagile.com>
2015-04-25 19:29:03 +0100

    Add interactive haskell support

--- src/22-haskell.el
+++ src/22-haskell.el
@@ -1,1 +1,10 @@
 (paradox-require 'haskell-mode)
+(paradox-require 'haskell-process)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t))
+
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
