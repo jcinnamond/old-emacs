6da0e3c09b8838c8840a0bcd527868f07ad3178e
John Cinnamond <jc@panagile.com>
2016-02-10 08:33:07 +0000

    Support for more languages

--- src/13-other-languages.el
+++ src/13-other-languages.el
@@ -3,3 +3,17 @@
 
 (paradox-require 'scss-mode)
 (setq scss-compile-at-save nil)
+
+(paradox-require 'haml-mode)
+
+;; Eugh! Javascript.
+(add-hook 'js-mode-hook 'flycheck-mode)
+(add-hook 'js-mode-hook 'flyspell-prog-mode)
+(add-hook 'js-mode-hook (lambda ()
+				  (setq indent-tabs-mode nil)
+				  (setq js-indent-level 2)))
+
+;; Yay! Elm.
+(paradox-require 'elm-mode)
+
+(paradox-require 'idris-mode)
