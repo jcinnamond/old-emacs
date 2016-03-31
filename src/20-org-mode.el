62ef64093defb74fd55f38cd20068f0020f15f78
John Cinnamond <jc@panagile.com>
2016-03-31 11:48:14 +0100

    Don't let org mode steal shift+arrow keys

--- src/20-org-mode.el
+++ src/20-org-mode.el
@@ -1,7 +1,14 @@
 (require 'org)
 (add-hook 'org-mode-hook (lambda()
 			   (auto-fill-mode)
-			   (flyspell-mode)))
+			   (flyspell-mode)
+
+			   ;; Unbind shift+arrows - I use these for moving between windows
+			   (define-key org-mode-map (kbd "S-<left>") nil)
+			   (define-key org-mode-map (kbd "S-<right>") nil)
+			   (define-key org-mode-map (kbd "S-<up>") nil)
+			   (define-key org-mode-map (kbd "S-<down>") nil)))
+
 (setq org-log-done t)
 (setq org-src-fontify-natively t)
 
